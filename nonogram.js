4752081426125af5794edc489dd5b8bf4c0e83f3
Agustin Rivera <agustin@rivera-web.com>
2018-09-28 11:01:47 -0700

    first real commit

---
+++ nonogram.js
@@ -0,0 +1,196 @@
+"use strict"
+
+
+var slider = document.getElementById("myDiff");
+var difficulty = slider.value; // medium default
+
+var gameBoard = {
+    grid: [], // ultimately a 3d grid
+    rowStreaks: [], // [i]=[]
+    colStreaks: [], // [i]=[]
+	diff: 50, // default
+	totalUsedSquares: 0,
+	length: 0
+}
+
+// by making an object we can add properties later without breaking 
+// existing code
+var square = {
+    isUsed: false,
+    isDisplayed: false
+}
+
+function startGame(length){
+	generateGrid(length);
+	createGameBoardHTML(length);
+	activateSection('play');
+}
+
+function setGameDifficulty(_difficulty){
+    difficulty=_difficulty;
+}
+
+// this seems redudant now but future features might add to this
+function createGameBoardHTML(length){
+	let board = document.getElementById('board');
+	let l = gameBoard.length + 1; // +1 for hints areas
+	let width = 100/l;
+
+	for (let i=0; i<l; i++){
+		for (let j=0; j<l; j++){
+			var square = document.createElement('div');
+			square.id=i+'|'+j;
+			square.className='square';
+			square.style='flex-basis: calc(' + width + '% - 4px);'
+
+			var sqContent = document.createElement('div');
+			sqContent.className='content';
+			square.appendChild(sqContent);
+			board.appendChild(square);
+		}
+	}
+}
+
+function generateGrid(l){
+	let row=[];
+	let totalSquaresUsed=0;
+    row.length=l;
+    for (let i=0; i<l; i++){
+        let col=[];
+        col.length=l;
+        for (let j=0; j<l; j++){
+			col[j]=copyObject(square);
+			if (isUsedSquare()){
+				col[j].isUsed=true;
+				totalSquaresUsed++;
+			}
+            col[j].isUsed=isUsedSquare();
+        }
+        row[i]=col;
+    }
+	gameBoard.grid=row;
+	gameBoard.totalUsedSquares=totalSquaresUsed;
+	gameBoard.diff=difficulty;
+	gameBoard.length=l;
+    showGridInConsole();
+    countStreaks();
+}
+
+function countStreaks(){
+    let l = gameBoard.grid[0].length;
+    for (let i=0; i<l; i++){
+        let colStreak=0;
+        let rowStreak=0;
+        let colStreaks = [];
+        let rowStreaks = [];
+        for (let j=0; j<l; j++){
+			// cols
+			// count up the consecutive marked spaces
+            if (gameBoard.grid[i][j].isUsed==true){
+                colStreak++
+            }
+            else {
+				// streak of marked spaces ended, push to array
+                if (colStreak>0){
+                    colStreaks.push(colStreak);
+                    colStreak=0;
+                }
+            }
+
+			// rows   just swap j and i - genius!
+			// count up the consecutive marked spaces
+            if (gameBoard.grid[j][i].isUsed==true){
+                rowStreak++
+            }
+            else {
+				// streak of marked spaces ended, push to array
+                if (rowStreak>0){
+                    rowStreaks.push(rowStreak);
+                    rowStreak=0;
+                }
+            }
+        }
+        // check for last streaks
+        if (colStreak>0){
+            colStreaks.push(colStreak);
+        }
+        if (rowStreak>0){
+            rowStreaks.push(rowStreak);
+        }
+
+        // makes multidimensional arrays e.g. colStreaks[0]=[]
+        gameBoard.colStreaks[i]=colStreaks;
+        gameBoard.rowStreaks[i]=rowStreaks;
+    }
+}
+
+
+slider.oninput = function() {
+    difficulty= this.value;
+}
+
+function isUsedSquare(){
+	// randomize the usage of a square, plus add some difficulty from the slider
+	let p = Math.floor(Math.random() * 100)/100;
+	let squareCutoff=difficultyMarkup()/100;
+    if (p<squareCutoff){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function difficultyMarkup(){
+	// don't let difficulty get too low or high
+	let diff=50;
+	if (difficulty<25){
+		diff=25
+	}
+	else if (difficulty>75){
+		diff=75;
+	}
+	else {
+		diff=difficulty;
+	}
+	return diff;
+}
+
+function activateSection(id){
+	console.log('Activating section: ' + id);
+    let main=document.getElementById('main');
+        var c = main.children;
+        for (let i = 0; i < c.length; i++) {
+            if (c[i].id != '')
+                document.getElementById(c[i].id).style.display = "none";
+        }
+    
+        document.getElementById(id).style.display = "inline";
+}
+
+function init(){
+    activateSection('welcome');
+}
+
+// ---------------- utility functions
+function copyObject(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+
+// ----------------- debug helpers
+function showGridInConsole(){
+	let l = gameBoard.grid[0].length;
+	console.log('Gameboard Stats -- ');
+	console.log('Diff: ' + gameBoard.diff);
+	console.log('Used Squares: ' + gameBoard.totalUsedSquares);
+    for (let i=0; i<l; i++) {
+        let colText = '';
+        for (let j=0; j<l; j++){
+            colText += gameBoard.grid[i][j].isUsed;
+            colText += ' ';
+        }
+        console.log(colText);
+    }
+}
+
+init();
